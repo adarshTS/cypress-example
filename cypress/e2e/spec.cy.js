e7fbde347dc1caab1b9fbaea2344fda40dc322e8
adarshTS <adarsh.ts8@gmail.com>
2022-11-02 01:22:16 +0530

    modified sample

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Bstack Demo', () => {
+  it('Filtering for apple devices', () => {
+    cy.visit('https://www.bstackdemo.com/')
+    cy.contains('Apple').click()
+    cy.contains('Add to cart').click()
   })
 })
